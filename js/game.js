0f098cd9d24745d02fd3321e7d9d1ab9fbd7aa04
Manuel A.H.E.J <manuelh3@illinois.edu>
2017-12-11 12:14:58 -0600

    Game Mechanics Complete
    Circle random color and position shift works, as well as shrinking. Need to finish start screen, score board, and end screen.

---
+++ js/game.js
@@ -0,0 +1,38 @@
+
+const AREA = document.body;
+var defaultLength = 75;
+var CIRCLE = document.querySelector('.circle');
+
+
+var circleSize = defaultLength;
+var clickCount = 0;
+
+var windowWidth = window.innerWidth - (circleSize + 2);
+var windowHeight = window.innerHeight - (circleSize + 2);
+
+var win = false;
+var lose = false;
+
+function positionCircle(e) {
+    var horizontalPosition = (windowWidth * Math.random()) + 1;
+    var verticalPosition= (windowHeight * Math.random()) + 1;
+    var newColor = randomColor();
+
+    circleSize = defaultLength;
+    // CIRCLE.style.height = defaultLength + 'px';
+    // CIRCLE.style.width = defaultLength + 'px';
+    CIRCLE.style.backgroundColor = newColor;
+    CIRCLE.style.left = horizontalPosition + 'px';
+    CIRCLE.style.top = verticalPosition + 'px';
+    setInterval(shrinkCircle, 100);
+}
+
+function shrinkCircle() {
+    circleSize -= 1 + (clickCount * 0.0001);
+    CIRCLE.style.height = circleSize + 'px';
+    CIRCLE.style.width = circleSize + 'px';
+    console.log("working");
+}
+
+CIRCLE.addEventListener('click', positionCircle, false);
+CIRCLE.addEventListener('click', function(){ clickCount += 1; console.log(clickCount) }, false);
