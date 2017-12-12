85637ceeb6907cc9e1b428f5d192d4bd04897c88
Manuel A.H.E.J <manuelh3@illinois.edu>
2017-12-11 22:33:33 -0600

    Changes to the Bad-Branch
    Cool

--- js/game.js
+++ js/game.js
@@ -10,8 +10,89 @@ var clickCount = 0;
 var windowWidth = window.innerWidth - (circleSize + 2);
 var windowHeight = window.innerHeight - (circleSize + 2);
 
-var win = false;
-var lose = false;
+var running = false;
+
+var main = document.getElementById('main');
+/**
+ * Hide element
+ */
+function hide(el) {
+  el.style.display = 'hide';
+}
+/**
+ * Show element
+ */
+function show(el) {
+  el.style.display = 'block';
+} 
+
+/**
+ * Show the main menu after loading all assets
+ */
+function mainMenu() {
+  show(main);
+}
+/**
+ * Click handlers for the different menu screens
+ */
+document.querySelectorAll('.play')[0].addEventListener('click', function() {
+  hide(main);
+  positionCircle();
+}, false);
+
+/**
+ * Click handlers for the game over
+ */
+function gameOver() {
+    running = false;
+    show(game-over);
+    show(score);
+}
+
+/**
+ * Click handlers for the restart
+ */
+document.querySelectorAll('.restart')[0].addEventListener('click', function() {
+    hide(game-over);
+    positionCircle();
+  }, false);
+
+// /**
+//  * End the game and restart
+//  */
+// function gameOver() {
+//     stop = true;
+//     $('#score').html(score);
+//     $('#game-over').show();
+//   }
+//   /**
+//    * Click handlers for the different menu screens
+//    */
+//   // …
+//   $('.restart').click(function() {
+//     $('#game-over').hide();
+//     startGame();
+//   });
+///////////////////////////////////
+// /**
+//  * Load the main menu
+//  */
+// assetLoader.finished = function() {
+//     mainMenu();
+//   }
+//   /**
+//    * Show the main menu after loading all assets
+//    */
+//   function mainMenu() {
+//     $('#main').show();
+//   }
+//   /**
+//    * Click handlers for the different menu screens
+//    */
+//   $('.play').click(function() {
+//     $('#menu').hide();
+//     startGame();
+//   });
 
 function positionCircle(e) {
     var horizontalPosition = (windowWidth * Math.random()) + 1;
@@ -28,10 +109,9 @@ function positionCircle(e) {
 }
 
 function shrinkCircle() {
-    circleSize -= 1 + (clickCount * 0.0001);
+    circleSize -= .5 + (clickCount * 0.000001);
     CIRCLE.style.height = circleSize + 'px';
     CIRCLE.style.width = circleSize + 'px';
-    console.log("working");
 }
 
 CIRCLE.addEventListener('click', positionCircle, false);
